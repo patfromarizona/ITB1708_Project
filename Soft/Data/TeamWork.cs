b2e80a28272141fb12378f2bc4782face48da027
Ilja <iljuhajr@gmail.com>
2022-01-30 19:29:44 +0200

    TeamSize property diplay on page set to "Team size"

--- Soft/Data/TeamWork.cs
+++ Soft/Data/TeamWork.cs
@@ -1,10 +1,14 @@
-﻿namespace Soft.Data
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Soft.Data
 {
     public class TeamWork
     {
         public string Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+
+        [Display(Name = "Team Size")]
         public int? TeamSize { get; set; }
         public DateTime? Deadline { get; set; }
         public bool? Done { get; set; }
