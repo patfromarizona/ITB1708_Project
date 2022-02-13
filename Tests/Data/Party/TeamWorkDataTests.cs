be65c38583eba534e029f7c7a76fae3b46755cd4
Ilja <iljuhajr@gmail.com>
2022-02-13 21:05:58 +0200

    TeamWorkData nad TeamWorkView test classes added GetRandom Aids class added

---
+++ Tests/Data/Party/TeamWorkDataTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using TeamUP.Aids;
+using TeamUP.Data.Party;
+
+namespace TeamUP.Tests.Data.Party
+{
+    [TestClass] public class TeamWorkDataTests: BaseTests<TeamWorkData> {
+        [TestMethod] public void TeamWorkIdTest() => isProperty<string>();
+        [TestMethod] public void NameTest() => isProperty<string?>();
+        [TestMethod] public void DescriptionTest() => isProperty<string?>();
+        [TestMethod] public void TeamSizeTest() => isProperty<int?>();
+        [TestMethod] public void DeadlineTest() => isProperty<DateTime?>();
+        [TestMethod] public void DoneTest() => isProperty<bool?>();
+    }
+
+    public class BaseTests<TClass>: AssertTests where TClass : class, new(){
+        protected TClass obj;
+        protected BaseTests() => obj = new TClass();
+        protected void isProperty<T>(T? value = default, bool isReadOnly = false)
+            => inconclusive();
+        private static bool isNullOrDefault<T>(T? value)
+            => value?.Equals(default(T)) ?? true;
+        /*private static bool canWrite(PropertyInfo i, bool isReadonly)
+        {
+
+        }*/
+        private static T random<T>() => GetRandom.Value<T>();
+        /*private string getCallingMember(string memberName)
+        {
+
+        }*/
+    }
+    public abstract class AssertTests {
+        protected static void inconclusive() => Assert.Inconclusive();
+        protected static void isNotNull([NotNull] object? o = null)
+            => Assert.IsNotNull(o);
+        protected static void areEqual(object? expected, object? actual) 
+            => Assert.AreEqual(expected, actual);
+    }
+}
---
+++ Tests/Facade/Party/TeamWorkViewTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TeamUP.Facade.Party;
+using TeamUP.Tests.Data.Party;
+
+namespace TeamUP.Tests.Facade.Party
+{
+    [TestClass]
+    public class TeamWorkViewTests: BaseTests<TeamWorkView>
+    {
+         
+        [TestMethod] public void TeamWorkIdTest() => isProperty<string>();
+        [TestMethod] public void NameTest() => isProperty<string?>();
+        [TestMethod] public void DescriptionTest() => isProperty<string?>();
+        [TestMethod] public void TeamSizeTest() => isProperty<int?>();
+        [TestMethod] public void DeadlineTest() => isProperty<DateTime?>();
+        [TestMethod] public void DoneTest() => isProperty < bool?>();
+        [TestMethod] public void OverviewTest() => isProperty<string? >();
+    }
+}
