6aa2aec53cf2c5788d7e0daa8518ae0c33b46775
Ilja <iljuhajr@gmail.com>
2022-02-17 20:10:31 +0200

    getCallingMember and isProperty test methods updated

--- Tests/Data/Party/TeamWorkDataTests.cs
+++ Tests/Data/Party/TeamWorkDataTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using TeamUP.Aids;
@@ -16,22 +17,40 @@ namespace TeamUP.Tests.Data.Party
         [TestMethod] public void DoneTest() => isProperty<bool?>();
     }
 
-    public class BaseTests<TClass>: AssertTests where TClass : class, new(){
+    public abstract class BaseTests<TClass>: AssertTests where TClass : class, new(){
         protected TClass obj;
         protected BaseTests() => obj = new TClass();
         protected void isProperty<T>(T? value = default, bool isReadOnly = false)
-            => inconclusive();
+        {
+            var memberName = getCallingMember(nameof(isProperty)).Replace("Test", string.Empty);
+            var propertyInfo = obj.GetType().GetProperty(memberName);
+            isNotNull(propertyInfo);
+            if (isNullOrDefault(value)) value = random<T>();
+            if (canWrite(propertyInfo, isReadOnly)) propertyInfo.SetValue(obj, value);
+            areEqual(value, propertyInfo.GetValue(obj));
+        }
         private static bool isNullOrDefault<T>(T? value)
             => value?.Equals(default(T)) ?? true;
-        /*private static bool canWrite(PropertyInfo i, bool isReadonly)
+        private static bool canWrite(PropertyInfo i, bool isReadonly)
         {
-
-        }*/
-        private static T random<T>() => Aids.GetRandom.Value<T>();
-        /*private string getCallingMember(string memberName)
+            var canWrite = i?.CanWrite?? false;
+            areEqual (canWrite, !isReadonly);
+            return canWrite;
+        }
+        private static T random<T>() => GetRandom.Value<T>();
+        private string getCallingMember(string memberName)
         {
-
-        }*/
+            var s = new StackTrace();
+            var isNext = false;
+            for (var i = 0; i < s.FrameCount - 1; i++)
+            {
+                var n = s.GetFrame(i)?.GetMethod()?.Name ?? string.Empty;
+                if (n is "MoveNext" or "Start") continue;
+                if (isNext) return n;
+                if (n == memberName) isNext = true;
+            }
+            return string.Empty;
+        }
     }
     public abstract class AssertTests {
         protected static void inconclusive() => Assert.Inconclusive();
