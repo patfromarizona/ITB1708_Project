39a5742dfa7dd7bbf63e658babf7bc344f36a584
Ilja <iljuhajr@gmail.com>
2022-02-18 14:04:33 +0200

    Deleted useless "using" lines in Facade/Party classes

--- Facade/Party/StudentView.cs
+++ Facade/Party/StudentView.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TeamUP.Facade.Party
 {
